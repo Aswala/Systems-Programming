b61d6afd1ed7fcaada2c14eaffd867af3f3d49ff
Aswala <99182236+Aswala@users.noreply.github.com>
2026-02-03 00:07:47 +0000

    Add socket and fork C programs

---
+++ fork.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid > 0)
+        printf("I am the parent\n");
+    else
+        printf("I am the child\n");
+
+    return 0;
+}
